381883d12fae302bfde08cf298627ab353f830b9
NPGonzalez <naypili@gmail.com>
2020-09-03 23:56:25 -0400

    Se crea componente de menú

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuCompnent';
 import './App.css';
+;
 
 class App extends Component {
 render() {
   return (
   <div className="App">
     <Navbar dark color="secondary">
-    <div className="container">
+    <div className="container-fluid">
       <NavbarBrand href="/">
         Restaurant Confusión
-      </NavbarBrand>
+         </NavbarBrand>
     </div>
     </Navbar>
-
+    <Menu />
     </div>
 );
   }
