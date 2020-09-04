0841c15241dbda290d2614fba78d209fe4f57a27
NPGonzalez <naypili@gmail.com>
2020-09-03 22:25:46 -0400

    Se agrega NavbarBrand

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import { Navbar } from 'reactstrap';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
 render() {
   return (
   <div className="App">
-    <Navbar dark color="primary"></Navbar>
+    <Navbar dark color="secondary">
+    <div className="container">
+      <NavbarBrand href="/">
+        Restaurant Confusión
+      </NavbarBrand>
+    </div>
+    </Navbar>
+
     </div>
 );
   }
